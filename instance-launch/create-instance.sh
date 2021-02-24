02efb108017a12487ea49fa03553425c604d48b1
KalluruS <>
2021-02-24 06:20:47 +0530

    Update ansible/

--- instance-launch/create-instance.sh
+++ instance-launch/create-instance.sh
@@ -24,6 +24,8 @@ envsubst <record.json >/tmp/${component}.json
 
 aws route53 change-resource-record-sets --hosted-zone-id Z076258917LZOFQ2TZ491 --change-batch file:///tmp/${component}.json
 
+
+### This is for inventory file
 sed -i -e "/${component}/ d" ../inventory
 PUBLIC_IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 echo "${PUBLIC_IPADDRESS} APP=${component}" >>../inventory
