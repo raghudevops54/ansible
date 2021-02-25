874cf2b4950b845d5d329c75679aea872e4f6899
KalluruS <>
2021-02-25 07:00:53 +0530

    Update ansible/

--- instance-launch/create-instance.sh
+++ instance-launch/create-instance.sh
@@ -7,6 +7,7 @@ component=$1
 if [ "${component}" == "all" ]; then
 
   for component in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment; do
+      echo "Creating ${component} Server"
       STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
       if [ "$STATE" != "running" ]; then
       aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0c43aaeb0e08199e0 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" &>/tmp/{component}.log
